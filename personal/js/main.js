2b8e78244084cc2c9b9ba4c8eb770ee313de7e7e
Ján Timoranský <j.timoransky@gmail.com>
2016-10-29 17:00:46 +0200

    Edited animations speeds and stuff

--- personal/js/main.js
+++ personal/js/main.js
@@ -9,12 +9,13 @@ $(function() {
 	    	$("#"+pageToAnimate+" > header").css("visibility", "visible");
     		$("#"+pageToAnimate+" > header").css("opacity", "1");
     		$("#"+pageToAnimate+" > header").css("top", "0");
-    		$("#"+pageToAnimate+" h1").addClass("border-bottom");
         }, 250);
+
 
 	    setTimeout(function(){
 	    	$("#"+pageToAnimate+" > *").css("visibility", "visible");
 	    	$("#"+pageToAnimate+" > *").css("opacity", "1");
+    		$("#"+pageToAnimate+" h1").addClass("border-bottom");
         }, 1000);    
 	}
 
@@ -34,14 +35,18 @@ $(function() {
         console.log('clicked show-' + event.target.innerHTML);
 
         $("#index h1").removeClass("border-bottom");
-        $("#index > header").css("top", "calc(50% - 101px)");
-        $("#index > *").css("opacity", "0");
-        $("#index").css("visibility", "hidden");
+
+        setTimeout(function(){
+        	$("#index > header").css("top", "calc(50% - 101px)");
+	        $("#index > *").css("opacity", "0");
+	        $("#index").css("visibility", "hidden");	
+        }, 500);
+        
         //$("#"+pageToAnimate+" header").css("display", "block");
 
         setTimeout(function(){
 	    	animatePageIn(event.target.innerHTML);
-        }, 1000);
+        }, 1200);
         
     });
 
@@ -50,15 +55,17 @@ $(function() {
         console.log(event.target.parentElement.id);
         
         $("#index h1, #about h1, #work h1, #contact h1").removeClass("border-bottom");
-        $("#index > header, #about > header, #work > header, #contact > header").css("top", "calc(50% - 101px)");
-        $("#about > *, #work > *, #contact > *").css("opacity", "0");
-        $("#about > *, #work > *, #contact > *").css("visibility", "hidden");
-        //
-        //$("#index").css("display", "flex");
+
+        setTimeout(function(){
+        	$("#index > header, #about > header, #work > header, #contact > header").css("top", "calc(50% - 101px)");
+		    $("#about > *, #work > *, #contact > *").css("opacity", "0");
+		    $("#about > *, #work > *, #contact > *").css("visibility", "hidden");	
+        }, 500);
+        
         setTimeout(function(){
         	$("#about, #work, #contact").css("display", "none");
 	    	animatePageIn('index');
-        }, 1000);
+        }, 1200);
     });
 
 
