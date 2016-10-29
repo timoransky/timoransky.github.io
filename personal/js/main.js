95e1646f159039cce978bb3097a288a39ac1ab3e
Ján Timoranský <j.timoransky@gmail.com>
2016-10-29 22:14:36 +0200

    Big workaround

--- personal/js/main.js
+++ personal/js/main.js
@@ -7,7 +7,8 @@ $(function() {
     	setTimeout(function(){
 	    	$("#" + pageToAnimate + " > header").css("visibility", "visible");
     		$("#" + pageToAnimate + " > header").css("opacity", "1");
-    		$("#" + pageToAnimate + " > header").css("top", "0");
+    		//$("#" + pageToAnimate + " > header").css("top", "0");
+    		$("#" + pageToAnimate + " > header").addClass("animate");
         }, 250);
 
 	    setTimeout(function(){
@@ -32,7 +33,8 @@ $(function() {
         }, 250); 
 
         setTimeout(function(){
-        	$("#" + pageOut + " > header").css("top", "calc(50% - 101px)");
+        	//$("#" + pageOut + " > header").css("top", "calc(50% - 101px)");
+        	$("#" + pageOut + " > header").removeClass("animate");
 		    $("#" + pageOut + " > *").css("opacity", "0");
 		    $("#" + pageOut + " > *").css("visibility", "hidden");	
         }, 500);
@@ -42,33 +44,6 @@ $(function() {
 	    	animatePageIn(pageIn);
         }, 1250);	
 	}
-/*
-	$(document).ready(function() {
-	    $('#fullpage').fullpage();
-	});
-/*
-	$.scrollify({
-		section : ".work-element",
-	});
-
-
-	$(".work-grid").onepage_scroll({
-		sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-		easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-		                                // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-		animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-		pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-		updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-		beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-		afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-		loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-		keyboard: false,                  // You can activate the keyboard controls
-		responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-		                                // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-		                                // the browser's width is less than 600, the fallback will kick in.
-		direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
-	});
-*/
 
 	$("#index > *, #about > *, #work > *, #contact > *").css("opacity", "0");
 	$("#index > *, #about > *, #work > *, #contact > *").css("visibility", "hidden");
