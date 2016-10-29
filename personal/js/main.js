dc407cf63ca3737af45d5cf39103c7b4bb4f2784
Ján Timoranský <j.timoransky@gmail.com>
2016-10-29 21:44:58 +0200

    Optimized javascript

--- personal/js/main.js
+++ personal/js/main.js
@@ -10,7 +10,6 @@ $(function() {
     		$("#" + pageToAnimate + " > header").css("top", "0");
         }, 250);
 
-
 	    setTimeout(function(){
 	    	$("#" + pageToAnimate + " > *").css("visibility", "visible");
 	    	$("#" + pageToAnimate + " > *").css("opacity", "1");
@@ -24,25 +23,52 @@ $(function() {
         }, 500);   
 	}
 
-	function animatePageOut(pageToAnimate) {
-		$("#" + pageToAnimate + " h1").removeClass("border-bottom");
-        $("#" + pageToAnimate + " .close").removeClass("rotate");
+	function animatePageOut(pageOut, pageIn) {
+		$("#" + pageOut + " h1").removeClass("border-bottom");
+        $("#" + pageOut + " .close").removeClass("rotate");
 
         setTimeout(function(){
-	    	$("#" + pageToAnimate + " .close").removeClass("grow");
+	    	$("#" + pageOut + " .close").removeClass("grow");
         }, 250); 
 
         setTimeout(function(){
-        	$("#" + pageToAnimate + " > header").css("top", "calc(50% - 101px)");
-		    $("#" + pageToAnimate + " > *").css("opacity", "0");
-		    $("#" + pageToAnimate + " > *").css("visibility", "hidden");	
+        	$("#" + pageOut + " > header").css("top", "calc(50% - 101px)");
+		    $("#" + pageOut + " > *").css("opacity", "0");
+		    $("#" + pageOut + " > *").css("visibility", "hidden");	
         }, 500);
         
         setTimeout(function(){
-        	$("#" + pageToAnimate).css("display", "none");
-	    	animatePageIn("index");
-        }, 1000);	
+        	$("#" + pageOut).css("display", "none");
+	    	animatePageIn(pageIn);
+        }, 1250);	
 	}
+/*
+	$(document).ready(function() {
+	    $('#fullpage').fullpage();
+	});
+/*
+	$.scrollify({
+		section : ".work-element",
+	});
+
+
+	$(".work-grid").onepage_scroll({
+		sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+		easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+		                                // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+		animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+		pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+		updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+		beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+		afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+		loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+		keyboard: false,                  // You can activate the keyboard controls
+		responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+		                                // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+		                                // the browser's width is less than 600, the fallback will kick in.
+		direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+	});
+*/
 
 	$("#index > *, #about > *, #work > *, #contact > *").css("opacity", "0");
 	$("#index > *, #about > *, #work > *, #contact > *").css("visibility", "hidden");
@@ -56,25 +82,10 @@ $(function() {
     }, 500);
 
 	$("#show-about, #show-work, #show-contact").click(function(event) {
-		$("#index h1").removeClass("border-bottom");
-
-        setTimeout(function(){
-        	$("#index > header").css("top", "calc(50% - 101px)");
-	        $("#index > *").css("opacity", "0");
-	        $("#index").css("visibility", "hidden");	
-        }, 500);
-        
-        setTimeout(function(){
-	    	animatePageIn(event.target.innerHTML);
-        }, 1000);
-        
+        animatePageOut("index", event.target.innerHTML)       
     });
 
     $(".go-back-button").click(function(event) {
-    	animatePageOut(event.target.parentElement.id);	
+    	animatePageOut(event.target.parentElement.id, "index");	
     });
-
-
-
-    
 });
