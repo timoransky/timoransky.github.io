dffedf37741ebc9c1a179e2972bbf562a6c07c8d
timoransky <j.timoransky@gmail.com>
2017-10-22 12:09:12 +0200

    Lazy loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-	$('.hero__name, .hero__position, .hero__foto_container').addClass('opacity');
+	$('.hero__name, .hero__position, .hero__links, .hero__foto_container').addClass('opacity');
 });
