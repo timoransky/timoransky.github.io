3e1a3921a32a52be13fb0bf46052a225c02aa6b2
timoransky <jan.timoransky@brackets.sk>
2022-10-13 21:39:41 +0200

    feat: change favicon

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,11 @@ export default function Document() {
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <link rel="preload" as="image" href="https://holopin.me/timoransky" />
+
+        <link
+          rel="shortcut icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👀</text></svg>"
+        />
       </Head>
       <body className="bg-gray-50 dark:bg-[#121212]">
         <Main />
